5a1c336d36e3b5875f2dfb37d5f73f48dc0abb74
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:09:34 +0300

    Add project files.

---
+++ Greatings/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введіть годину (0-23): ");
+int h = int.Parse(Console.ReadLine());
+
+if (h >= 5 && h < 12)
+{
+    Console.WriteLine("Доброго ранку!");
+}
+else if (h >= 12 && h < 18)
+{
+    Console.WriteLine("Добрий день!");
+}
+else if (h >= 18 && h < 22)
+{
+    Console.WriteLine("Добрий вечір!");
+}
+else
+{
+    Console.WriteLine("Доброї ночі!");
+}
